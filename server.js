340316f0950a282e0e19df970f7d75cd5d682df5
Ezgi AKIN <karvanezgi@gmail.com>
2021-01-29 13:04:00 +0300

    port updated

--- server.js
+++ server.js
@@ -8,14 +8,12 @@ const restaurants = require('./api/restaurants');
 const errorHandler = require('./middleware/error');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-if (process.env.NODE_ENV === 'dev') {
-  app.use(cors());
-}
 
+app.use(cors());
 app.use(express.json());
 app.use('/restaurant', restaurants);
 app.use(errorHandler);
